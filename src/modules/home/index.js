1e25fa4e47dea0556874593063aebb36fd3149f3
kdsinghak <kuldeep.singh1@appinventiv.com>
2022-09-20 17:13:12 +0530

    code optimize

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,13 +1,17 @@
 import {styles} from './styles';
 import {
-  edgePadding,
+  moveTo,
+  showRoute,
+  traceRoute,
   mapViewProps,
   LATITUDE_DELTA,
   LONGITUDE_DELTA,
   GOOGLE_MAPS_APIKEY,
+  onPlaceSelected,
+  traceRouteOnReady,
 } from './utils';
+import React, {useState, useRef} from 'react';
 import localImages from '../../utils/localImages';
-import React, {useState, useRef, useEffect} from 'react';
 import MapViewDirections from 'react-native-maps-directions';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import {
@@ -33,49 +37,14 @@ export default function Home() {
   const [modalVisible, setModalVisible] = useState(false);
   const [showDirection, setShowDirection] = useState(false);
 
-  const showRoute = () => {
-    Animated.timing(animate, {
-      toValue: 1,
-      duration: 500,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const traceRoute = () => {
-    if (origin && destination) {
-      setShowDirection(true);
-      mapRef.current?.fitToCoordinates([origin, destination], {edgePadding});
-    }
-    setModalVisible(false);
-    showRoute();
-  };
-
-  const moveTo = async position => {
-    const camera = await mapRef.current?.getCamera();
-    if (camera) {
-      camera.center = position;
-      mapRef.position.animateCamera(camera, {duration: 1000});
-    }
-  };
-
   const traceRouteOnReady = args => {
     if (args) {
       setDistance(args.distance);
       setDuration(args.duration);
     }
   };
-
-  const onPlaceSelected = (
-    details: GooglePlaceDetail | null,
-    flag: 'origin' | 'destination',
-  ) => {
-    const set = flag === 'origin' ? setOrigin : setDestination;
-    const position = {
-      latitude: details?.geometry?.location?.lat || 0,
-      longitude: details?.geometry?.location?.lng || 0,
-    };
-    set(position);
-    moveTo(position);
+  const handleModal = () => {
+    setModalVisible(!modalVisible);
   };
 
   return (
@@ -95,32 +64,32 @@ export default function Home() {
           icon={localImages.locationPin}
         />
       </View>
+
       <MapView
         ref={mapRef}
-        mapType="standard"
-        maxZoomLevel={20}
         minZoomLevel={5}
+        maxZoomLevel={20}
         {...mapViewProps}
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        mapType="standard"
         style={styles.map}
-        initialRegion={loc}>
+        initialRegion={loc}
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      >
         {loc && <Marker coordinate={loc} />}
         {origin && <Marker coordinate={origin} />}
         {destination && <Marker coordinate={destination} />}
         {showDirection && origin && destination && (
           <MapViewDirections
+            strokeWidth={7}
             origin={origin}
+            strokeColor="#02B0FF"
             destination={destination}
             apikey={GOOGLE_MAPS_APIKEY}
-            strokeColor="#02B0FF"
-            strokeWidth={7}
             onReady={traceRouteOnReady}
           />
         )}
       </MapView>
-      <TouchableOpacity
-        onPress={() => setModalVisible(!modalVisible)}
-        style={styles.directionView}>
+      <TouchableOpacity onPress={handleModal} style={styles.directionView}>
         <Image source={localImages.direction} style={styles.icon} />
       </TouchableOpacity>
 
@@ -128,17 +97,21 @@ export default function Home() {
         animationType="slide"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}>
+        onRequestClose={handleModal}>
         <TouchableOpacity
-          onPress={() => setModalVisible(!modalVisible)}
+          onPress={handleModal}
           style={{flex: 1, backgroundColor: 'transparent'}}>
           <View style={styles.searchContainer}>
             <InputAutocomplete
               placeholder="Choose Source"
               onPlaceSelected={details => {
-                onPlaceSelected(details, 'origin');
+                onPlaceSelected(
+                  details,
+                  'origin',
+                  setOrigin,
+                  setDestination,
+                  mapRef,
+                );
               }}
               Styles={styles.direction}
               icon={localImages.gps}
@@ -148,7 +121,13 @@ export default function Home() {
             <InputAutocomplete
               placeholder="Choose Destination"
               onPlaceSelected={details => {
-                onPlaceSelected(details, 'destination');
+                onPlaceSelected(
+                  details,
+                  'destination',
+                  setOrigin,
+                  setDestination,
+                  mapRef,
+                );
               }}
               Styles={styles.direction}
               icon={localImages.locationPin}
@@ -156,40 +135,49 @@ export default function Home() {
             <TouchableOpacity
               style={styles.button}
               activeOpacity={0.6}
-              onPress={traceRoute}>
+              onPress={() =>
+                traceRoute(
+                  mapRef,
+                  origin,
+                  destination,
+                  animate,
+                  setShowDirection,
+                  setModalVisible,
+                  showRoute,
+                )
+              }>
               <Text style={styles.buttonText}>{'Show Route'}</Text>
             </TouchableOpacity>
           </View>
         </TouchableOpacity>
       </Modal>
 
-      {/* {distance && duration ? ( */}
-      <Animated.View
-        style={{
-          height: 120,
-          width: '100%',
-          marginTop: 'auto',
-          backgroundColor: 'white',
-
-          transform: [
-            {
-              translateY: animate.interpolate({
-                inputRange: [0, 1],
-                outputRange: [120, 0],
-              }),
-            },
-          ],
-        }}>
-        <View style={styles.distanceView}>
-          <Text style={styles.TimeAndDistance}>{`${Math.ceil(
-            duration,
-          )}min (${distance.toFixed(2)})km`}</Text>
-          <Text style={styles.quote}>
-            {'Fastest route now due to traffic conditions'}
-          </Text>
-        </View>
-      </Animated.View>
-      {/* ) : null} */}
+      {distance && duration ? (
+        <Animated.View
+          style={{
+            height: 120,
+            width: '100%',
+            marginTop: 'auto',
+            backgroundColor: 'white',
+            transform: [
+              {
+                translateY: animate.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [120, 0],
+                }),
+              },
+            ],
+          }}>
+          <View style={styles.distanceView}>
+            <Text style={styles.TimeAndDistance}>{`${Math.ceil(
+              duration,
+            )}min (${distance.toFixed(2)})km`}</Text>
+            <Text style={styles.quote}>
+              {'Fastest route now due to traffic conditions'}
+            </Text>
+          </View>
+        </Animated.View>
+      ) : null}
     </View>
   );
 }
--- src/modules/home/utils.js
+++ src/modules/home/utils.js
@@ -1,5 +1,7 @@
-import {Dimensions} from 'react-native';
 export const GOOGLE_MAPS_APIKEY = 'AIzaSyBsMTfGLTXlY9QI__OLaBVetpd7WE8-qWY';
+import {GooglePlaceDetail} from 'react-native-google-places-autocomplete';
+import {Animated} from 'react-native';
+import {Dimensions} from 'react-native';
 const edgePaddingValue = 40;
 export const edgePadding = {
   top: edgePaddingValue,
@@ -13,12 +15,72 @@ const ASPECT_RATIO = width / height;
 export const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export const mapViewProps = {
-  zoomTapEnabled: true,
   zoomEnabled: true,
+  showsTraffic: true,
   scrollEnabled: true,
   rotateEnabled: true,
   loadingEnabled: true,
+  zoomTapEnabled: true,
   showsUserLocation: true,
   showsMyLocationButton: true,
-  showsTraffic: true,
+};
+
+export const showRoute = animate => {
+  Animated.timing(animate, {
+    toValue: 1,
+    duration: 500,
+    useNativeDriver: true,
+  }).start();
+};
+
+export const moveTo = async (position, mapRef) => {
+  const camera = await mapRef.current?.getCamera();
+  if (camera) {
+    camera.center = position;
+    mapRef.position.animateCamera(camera, {duration: 1000});
+  }
+};
+
+export const traceRoute = (
+  mapRef,
+  origin,
+  destination,
+  animate,
+  setShowDirection,
+  setModalVisible,
+  showRoute,
+) => {
+  if (origin && destination) {
+    setShowDirection(true);
+    mapRef.current?.fitToCoordinates([origin, destination], {edgePadding});
+  }
+  setModalVisible(false);
+  showRoute(animate);
+};
+
+export const traceRouteOnReady = (args, setDistance, setDuration) => {
+  if (args) {
+    setDistance(args.distance);
+    setDuration(args.duration);
+  }
+};
+export const onPlaceSelected = (
+  details: GooglePlaceDetail | null,
+  flag: 'origin' | 'destination',
+
+  setOrigin,
+  setDestination,
+  mapRef,
+) => {
+  const set = flag === 'origin' ? setOrigin : setDestination;
+  const position = {
+    latitude: details?.geometry?.location?.lat || 0,
+    longitude: details?.geometry?.location?.lng || 0,
+  };
+  set(position);
+  moveTo(position, mapRef);
+};
+
+const handleModal = () => {
+  setModalVisible(!modalVisible);
 };
